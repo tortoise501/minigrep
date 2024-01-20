b991e4b7f4159eb9410a5b54db594c2f477381c0
tortoise501 <tralala5.555.d@gmail.com>
2024-01-20 19:44:38 +0100

    added new() function for config

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     
     println!("Searching for {}",config.query);
     println!("In file{} ",config.filename);
@@ -21,12 +21,13 @@ struct Config{
     query: String,
     filename: String,
 }
-
-fn parse_config(args: &[String]) -> Config{
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config{
-        query,
-        filename,
+impl Config{
+    fn new(args: &[String]) -> Config{
+        let query = args[0].clone();
+        let filename = args[1].clone();
+        Config{
+            query,
+            filename,
+        }
     }
 }

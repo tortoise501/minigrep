6069d8a338706a973875ca16ca9a5b01432f7d94
tortosie501 <tralala5.555.d@gmail.com>
2024-01-20 19:20:26 +0100

    added configuration structure

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,30 @@ use std::fs::File;
 use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
     
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Searching for {}",config.query);
+    println!("In file{} ",config.filename);
     
-    println!("Searching for {}",query);
-    println!("In file{} ",filename);
-    
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
     
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
 
     println!("With text:\n{}",contents);
     
+}
+
+struct Config{
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config{
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config{
+        query,
+        filename,
+    }
 }
